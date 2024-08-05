0985a53e5c11f754f8ff673b0e530c551b718f4a
joel-ajp <andrewjoelporter@gmail.com>
2024-08-05 12:39:51 -0500

    work

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Joel-ajp/netsuite-middleware
 
 go 1.23
+
+require github.com/texttheater/golang-levenshtein/levenshtein v0.0.0-20200805054039-cae8b0eaed6c // indirect
--- main.go
+++ main.go
@@ -6,11 +6,99 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
 	"os/exec"
+	"strings"
+
+	"github.com/texttheater/golang-levenshtein/levenshtein"
 )
 
-func create_folder(data map[string]interface{}) {
-	return
+func get_directories(path string) ([]string, error) {
+	files, err := os.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	var directories []string
+	for _, file := range files {
+		if file.IsDir() {
+			directories = append(directories, strings.ToLower(file.Name()))
+		}
+	}
+	return directories, nil
+}
+
+func findBestMatch(clientName string, directories []string) string {
+	clientName = strings.ToLower(clientName)
+	bestMatch := ""
+	minDistance := len(clientName) + 1
+
+	for _, dir := range directories {
+		distance := levenshtein.DistanceForStrings([]rune(clientName), []rune(dir), levenshtein.DefaultOptions)
+		if distance < minDistance {
+			minDistance = distance
+			bestMatch = dir
+		}
+	}
+
+	return bestMatch
+}
+
+func get_client(client string, root string) (string, error) {
+	clients, err := get_directories(root)
+	if err != nil {
+		return "", err
+	}
+
+	bestMatch := findBestMatch(client, clients)
+	return bestMatch, nil
+}
+
+func create_folder(jsondata map[string]interface{}) (string, error) {
+	ROOT_PATH := `/mnt/Survey/Projects`
+	TEMPLATE_PATH := `/mnt/Survey/Standards_Templates/8.0 DFS Directory Structure/FE Folder Structure/`
+	client, err := get_client(jsondata["client"].(string), ROOT_PATH)
+	if err != nil {
+		return "", err
+	}
+	project := jsondata["project"].(string)
+
+	dest_path := fmt.Sprintf("%s/%s/%s", ROOT_PATH, client, project)
+	if err != nil {
+		return "", err
+	}
+
+	cmd := exec.Command("rsync", "-a", TEMPLATE_PATH, dest_path)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return "", fmt.Errorf("Error creating folder: %s Output: %s", err, string(output))
+	}
+
+	return string(output), nil
+}
+
+func check_json(jsondata map[string]interface{}) string {
+	_, clientNameExists := jsondata["client"]
+	_, projectNameExists := jsondata["project"]
+	_, subsidiaryExists := jsondata["subsidiary"]
+	_, formTypeExists := jsondata["form_type"]
+
+	var missingElements []string
+	if !clientNameExists {
+		missingElements = append(missingElements, "client")
+	}
+	if !projectNameExists {
+		missingElements = append(missingElements, "project")
+	}
+	if !subsidiaryExists {
+		missingElements = append(missingElements, "subsidiary")
+	}
+	if !formTypeExists {
+		missingElements = append(missingElements, "form_type")
+	}
+	missing := strings.Join(missingElements, " ")
+
+	return missing
 }
 
 func post_to_server(w http.ResponseWriter, r *http.Request) {
@@ -29,37 +117,23 @@ func post_to_server(w http.ResponseWriter, r *http.Request) {
 	var jsonData map[string]interface{}
 	err = json.Unmarshal(body, &jsonData)
 	if err != nil {
-		http.Error(w, "Error marshalling to json:", http.StatusInternalServerError)
+		http.Error(w, "Error unmarshalling to json:", http.StatusInternalServerError)
 		return
 	}
 
-	clientName, clientNameExists := jsonData["client"]
-	projectName, projectNameExists := jsonData["project"]
-	subsidiary, subsidiaryExists := jsonData["subsidiary"]
-	formType, formTypeExists := jsonData["form_type"]
-
-	if !clientNameExists || !projectNameExists || !subsidiaryExists || !formTypeExists {
-		var missing string = ""
-
-		if !clientNameExists {
-			missing += "client "
-		}
-		if !projectNameExists {
-			missing += "project "
-		}
-		if !subsidiaryExists {
-			missing += "subsidiary "
-		}
-		if !formTypeExists {
-			missing += "form_type "
-		}
-
+	missing := check_json(jsonData)
+	if missing != "" {
 		http.Error(w, fmt.Sprintf("Missing the following json fields: %s", missing), http.StatusBadRequest)
 		return
+	} else {
+		output, err := create_folder(jsonData)
+		if err != nil {
+			http.Error(w, fmt.Sprintf("Error creating folder: %s", err), http.StatusInternalServerError)
+			return
+		} else {
+			fmt.Fprintf(w, "Folder created successfully: %s", output)
+		}
 	}
-
-	fmt.Fprintf(w, "Client name:%s,Project name:%s,Subsidiary:%s,Form Type:%s", clientName, projectName, subsidiary, formType)
-	// Create folder on the server
 }
 
 func main() {
